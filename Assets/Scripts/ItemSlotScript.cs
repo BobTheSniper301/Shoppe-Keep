e9a9be073c6d151fd7676a644c380492bfa59318
BobTheSniper301 <owenspeyton159@gmail.com>
2025-10-01 15:05:06 -0500

    More work on item slot data
    Made some loops in my ui manager, added a save button. Still broken, look into slotnum instead of item num maybe, doubful nonetheless

--- Assets/Scripts/ItemSlotScript.cs
+++ Assets/Scripts/ItemSlotScript.cs
@@ -14,15 +14,22 @@ public class ItemSlotScript : MonoBehaviour, IDropHandler
             ItemScript item = eventData.pointerDrag.GetComponent<ItemScript>();
             item.parentAfterDrag = transform;
             item.itemData.itemNum = slotNum;
+            //Debug.Log(slotNum);
         }
         else if (transform.childCount == 1)
         {
             ItemScript slottedItem = gameObject.GetComponentInChildren<ItemScript>();
             ItemScript item = eventData.pointerDrag.GetComponent<ItemScript>();
+            // updates the items slot number
             slottedItem.itemData.itemNum = item.itemData.itemNum;
             item.itemData.itemNum = slotNum;
+            
+            // Reparents
             slottedItem.transform.SetParent(item.parentAfterDrag);
             item.parentAfterDrag = transform;
+
+            //Debug.Log(slotNum);
+
             item.OnEndDrag(eventData);
         }
         
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,21 +6,52 @@ using System.Collections.Generic;
 public class UIManager : MonoBehaviour
 {
     public ItemScript[] items;
-    public int[] itemSlotData = new int[] {0,0,0,0,0,0,0,0};
+    public ItemSlotScript[] itemSlots;
+
+    public void Save()
+    {
+        // Gets a list of all item slots
+        int x = 0;
+        itemSlots = GetComponentsInChildren<ItemSlotScript>();
+        // Check each of the items slots to see if they have an item in them
+        foreach (ItemSlotScript i in itemSlots)
+        {
+             //If it does have an item, if it does, it stores it into the items list at the proper index
+            if (i.GetComponentInChildren<ItemScript>())
+            {
+                items[x] = i.GetComponentInChildren<ItemScript>();
+            }
+            //Debug.Log(string.Join(", ", items[x]));
+            x++;
+        }
+
+
+        // Checks each thing in items array, if it's an item it checks the items slot number and if it doesn't match it removes it
+        int y = 1;
+        while (y < items.Length)
+        {
+            if (items[y] is ItemScript)
+            {
+                if (items[y].itemData.itemNum != y)
+                {
+                    items[y - 1] = null;
+                }
+            }
+            y++;
+        }
+
+        // Prints all items within the items list
+        int z = 0;
+        while (z < items.Length)
+        {
+            //Debug.Log(items[z]);
+            z++;
+        }
+    }    
 
     void Start()
     {
-        items = GetComponentsInChildren<ItemScript>();
-        Debug.Log(items.Length);
-        //foreach (Component item in items)
-        for (int i = 0; i < items.Length; i++)
-        {
-            Debug.Log(itemSlotData.Length);
-            Debug.Log(i);
-            itemSlotData.SetValue(items[i].itemData.itemNum, i);
-            Debug.Log(itemSlotData);
 
-        }
     }
 
 }
