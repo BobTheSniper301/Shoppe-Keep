d1f74fe2f9fb7301be9b938c95eec0bf54c0e19d
BobTheSniper301 <owenspeyton159@gmail.com>
2025-10-03 15:03:11 -0500

    Cleaned
    Removed Item num, made item slot script a bit better

--- Assets/Scripts/ItemSlotScript.cs
+++ Assets/Scripts/ItemSlotScript.cs
@@ -5,33 +5,34 @@ using UnityEngine.EventSystems;
 public class ItemSlotScript : MonoBehaviour, IDropHandler
 {
     public int slotNum;
-  
+
+    public ItemScript item;
+    public ItemScript slottedItem;
 
     public void OnDrop(PointerEventData eventData)
-    {
+    { 
+        ItemScript item = eventData.pointerDrag.GetComponent<ItemScript>();
+        ItemScript slottedItem = gameObject.GetComponentInChildren<ItemScript>();
+
         if (transform.childCount == 0)
         {
-            ItemScript item = eventData.pointerDrag.GetComponent<ItemScript>();
             item.parentAfterDrag = transform;
-            item.itemData.itemNum = slotNum;
-            //Debug.Log(slotNum);
         }
         else if (transform.childCount == 1)
-        {
-            ItemScript slottedItem = gameObject.GetComponentInChildren<ItemScript>();
-            ItemScript item = eventData.pointerDrag.GetComponent<ItemScript>();
-            // updates the items slot number
-            slottedItem.itemData.itemNum = item.itemData.itemNum;
-            item.itemData.itemNum = slotNum;
-            
+        {   
             // Reparents
             slottedItem.transform.SetParent(item.parentAfterDrag);
             item.parentAfterDrag = transform;
 
-            //Debug.Log(slotNum);
 
             item.OnEndDrag(eventData);
         }
         
     }
+
+    private void Start()
+    {
+        
+    }
+
 }
