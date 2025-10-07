3fd1b449172bf7be1b6e67c0197908b97a79e678
BobTheSniper301 <owenspeyton159@gmail.com>
2025-10-06 20:06:00 -0500

    .

---
+++ Assets/Scripts/InventoryData.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public class InventoryData
+{
+    public ItemScript[] SavedItems = new ItemScript[8];
+    
+
+}
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,14 +1,20 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 
 
 public class UIManager : MonoBehaviour
 {
+    // private InventoryData inventoryData;
+
+    // All items the player has and at index matching the hotbar
     public ItemScript[] items;
+
     public ItemSlotScript[] itemSlots;
 
-    public void Save()
+    // Gets all items in the hotbar and saves them to items[] at an index matching the hotbar slot
+    void GetItems()
     {
         // Gets a list of all item slots
         int x = 0;
@@ -16,7 +22,7 @@ public class UIManager : MonoBehaviour
         // Check each of the items slots to see if they have an item in them
         foreach (ItemSlotScript i in itemSlots)
         {
-             //If it does have an item, if it does, it stores it into the items list at the proper index
+            //If it does have an item, if it does, it stores it into the items list at the proper index
             if (i.GetComponentInChildren<ItemScript>())
             {
                 items[x] = i.GetComponentInChildren<ItemScript>();
@@ -25,33 +31,64 @@ public class UIManager : MonoBehaviour
             x++;
         }
 
+        // // Prints all items within the items list
+        // int z = 0;
+        // while (z < items.Length)
+        // {
+        //     Debug.Log(items[z]);
+        //     z++;
+        // }
+    }
 
-        // Checks each thing in items array, if it's an item it checks the items slot number and if it doesn't match it removes it
-        int y = 1;
-        while (y < items.Length)
-        {
-            if (items[y] is ItemScript)
-            {
-                if (items[y].itemData.itemNum != y)
-                {
-                    items[y - 1] = null;
-                }
-            }
-            y++;
-        }
+    // void SetInventoryData(int i)
+    // {
+    //     inventoryData.SavedItems[i] = items[i];   
+    // }
 
-        // Prints all items within the items list
-        int z = 0;
-        while (z < items.Length)
-        {
-            //Debug.Log(items[z]);
-            z++;
-        }
-    }    
-
-    void Start()
+    public void Save()
     {
+        GetItems();
+        // int x = 0;
+        // while (x < items.Length)
+        // {
+        //     inventoryData.SavedItems[x] = items[x];
+        // }
+        // Debug.Log(inventoryData.SavedItems + "hi");
+        Debug.Log(items + "hi");
 
+        // inventoryData.SavedItems = items;
+
+        
+
+        // string json = JsonUtility.ToJson(inventoryData);
+        // Debug.Log(json);
+
+        using (StreamWriter writer = new StreamWriter(Application.dataPath + Path.AltDirectorySeparatorChar + "InventoryData.json"))
+        {
+            // writer.Write(json);
+        }
+
+    }
+
+
+    void Load()
+    {
+        string json = string.Empty;
+
+        using (StreamReader reader = new StreamReader(Application.dataPath + Path.AltDirectorySeparatorChar + "InventoryData.json"))
+        {
+            json = reader.ReadToEnd();
+        }
+
+        InventoryData data = JsonUtility.FromJson<InventoryData>(json);
+        
+        // items = inventoryData.SavedItems; 
+    }
+
+
+    private void Start()
+    {
+        Load();
     }
 
 }
